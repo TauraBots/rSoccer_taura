9f034e9807b6616a40cbb1fdf106471ff6651e59
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2023-05-15 17:07:00 -0300

    Consertado path e adicionado codigos para 1x0 e 1x3

--- rsoccer_gym/vss/env_vss/__init__.py
+++ rsoccer_gym/vss/env_vss/__init__.py
@@ -1,2 +1,4 @@
-from rsoccer_gym.vss.env_vss.vss_gym import VSSEnv
-from rsoccer_gym.vss.env_vss.vss_gym_fira import VSSFIRAEnv
+from rSoccer_taura.rsoccer_gym.vss.env_vss.vss_gym import VSSEnv
+from rSoccer_taura.rsoccer_gym.vss.env_vss.vss_gym_fira import VSSFIRAEnv
+from rSoccer_taura.rsoccer_gym.vss.env_vss.vss_1x3 import VSS1x3
+from rSoccer_taura.rsoccer_gym.vss.env_vss.vss_1x0 import VSS1x0
