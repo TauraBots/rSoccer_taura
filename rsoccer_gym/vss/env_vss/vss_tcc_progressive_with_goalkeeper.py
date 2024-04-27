21a1e87d150e1a9ac32cd71973d22e20914275cd
iOsnaaente <bruno.sampaio@acad.ufsm.br>
2024-04-27 11:18:41 -0300

    Ajuste do goleiro para seguir pontos aleatórios

--- rsoccer_gym/vss/env_vss/vss_tcc_progressive_with_goalkeeper.py
+++ rsoccer_gym/vss/env_vss/vss_tcc_progressive_with_goalkeeper.py
@@ -15,6 +15,7 @@ from typing import Dict
 import numpy as np
 import pickle
 import random
+import time 
 import math
 import gym
 
@@ -34,20 +35,18 @@ def menor_angulo(v1, v2):
         return -angle
     return angle
 
-
 def transform(v1, ang):
     mod = np.sqrt(v1[0] ** 2 + v1[1] ** 2)
     v1 = (v1[0] / mod, v1[1] / mod)
-
     mn = menor_angulo(v1, (math.cos(ang), math.sin(ang)))
-
     return mn, (math.cos(mn) * mod, math.sin(mn) * mod)
+
+
 
 
 class vss_tcc_progressive_with_goalkeeper(VSSBaseEnv):
     """This environment controls a singl
     e robot in a VSS soccer League 3v3 match
-
 
         Description:
         Observation:
@@ -87,35 +86,43 @@ class vss_tcc_progressive_with_goalkeeper(VSSBaseEnv):
             5 minutes match time
     """
 
+    # go_to_point: list = [ ]
+    point1 = [ random.random()*( -1 if random.randint(0,1) == 1 else 1), random.random()*( -1 if random.randint(0,1) == 1 else 1) ]         
+    point2 = [ random.random()*( -1 if random.randint(0,1) == 1 else 1), random.random()*( -1 if random.randint(0,1) == 1 else 1) ]         
+    point3 = [ random.random()*( -1 if random.randint(0,1) == 1 else 1), random.random()*( -1 if random.randint(0,1) == 1 else 1) ]         
+
+    t1 = 0.0
+
     def __init__(self):
         super().__init__(
             field_type=0, n_robots_blue=1, n_robots_yellow=3, time_step=0.025
         )
 
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.action_space = gym.spaces.Box( low = -1, high = 1, shape = (2,), dtype = np.float32 )
+        
         self.observation_space = gym.spaces.Box(
-            low=-self.NORM_BOUNDS, high=self.NORM_BOUNDS, shape=(17,), dtype=np.float32
+            low = -self.NORM_BOUNDS, high = self.NORM_BOUNDS, shape = (17,), dtype = np.float32
         )
 
+        # O que isso faz ?
         self.goleiro_teve_bola = False
 
         # Initialize Class Atributes
         self.previous_ball_potential = None
-        self.actions: Dict = None
+
         self.reward_shaping_total = None
-        self.v_wheel_deadzone = 0.05
+        self.v_wheel_deadzone: float = 0.05
+        self.actions:dict = None
 
         self.ou_actions = []
         for _ in range(self.n_robots_blue + self.n_robots_yellow):
             self.ou_actions.append(
                 OrnsteinUhlenbeckAction(self.action_space, dt=self.time_step)
             )
-
         self.difficulty = 0.0  # starts easy
-
         self.plotting_data = []
-
         print("Environment initialized")
+
 
     def set_diff(self, mean_rewards, max_mean_rewards=450):
         diff = min(
@@ -141,10 +148,15 @@ class vss_tcc_progressive_with_goalkeeper(VSSBaseEnv):
         self.plotting_data.append([(0, 0)])
 
         # Falta colocar o goleiro amarelo na reta do gol e controlar ele a partir dessa posição
-        # self.
+        self.t1 = time.time( )
+        self.next_point()
 
         return super().reset()
+    
 
+    def next_point( self ):
+        self.target = [ 0.6*random.random()*( -1 if random.randint(0,1) == 1 else 1), 0.6*random.random()*( -1 if random.randint(0,1) == 1 else 1) ]         
+    
     def step(self, action):
         if self.plotting_data[-1][-1] != (
             self.frame.robots_blue[0].x,
@@ -162,42 +174,53 @@ class vss_tcc_progressive_with_goalkeeper(VSSBaseEnv):
     def observations_atacante(self):
         return observations(self)
 
-    def _get_goalkeeper_vels(self):
-        # Obter a posição atual do goleiro
-        gk_pos = self.frame.robots_yellow[0]
+    def _get_goalkeeper_vels( self ) -> list:
+        # Pegar a posição do Goleiro 
+        gk: Robot = self.frame.robots_yellow[0]     # Obter o goleiro
+        gk_pos: list = [ gk.x, gk.y ]               # Obter a posição do goleiro 
+        gk_angle: float = math.radians(gk.theta)    # Calcular a orientação do goleiro em radianos
+        gk_v_wheel: list = [ 0, 0 ]                 # Referencia de velocidade do goleiro
 
-        # Obter a posição da bola
-        ball_pos = self.frame.ball
+        
+        # Pegar a posição da bola  
+        ball: Ball = self.frame.ball                # Obter a bola
+        ball_pos: list = [ ball.x, ball.y ]         # Obter a posição da bola
 
-        # Calcular a diferença entre a posição Y do goleiro e a posição Y da bola
-        diff_y = ball_pos.y - gk_pos.y
+        # Cria um target para ir até lá
+        # ball_pos: list = [ 0.6, 0.50 ]        
+        if time.time() - self.t1 > 2:
+            ball_pos = self.target  
+            if np.sqrt((gk_pos[0] - ball_pos[0]) ** 2 + (gk_pos[1] - ball_pos[1]) ** 2) < 0.05:
+                self.next_point()
+        
+        # Pegar a distancia entre o goleiro e a bola  
+        robot2ball_diff: list = [ ball_pos[0] - gk_pos[0], ball_pos[1] - gk_pos[1] ]            # return [ dx, dy ]
+        robot2ball_mag: float = np.sqrt((robot2ball_diff[0]) ** 2 + (robot2ball_diff[1]) ** 2)  # return scalar 
+        robot2ball_ang: float = np.arctan2( robot2ball_diff[1], robot2ball_diff[0])             # return scalar between [-pi, pi ]
 
-        # Calcular a orientação do goleiro em radianos
-        gk_orientation = math.radians(gk_pos.orientation)
+        # Calcula a diferença entre o angulo do robo e o angulo gerado entre o robo e a bola 
+        diff_ang = gk_angle - robot2ball_ang 
+        print( gk_pos, robot2ball_diff)
 
-        # Definir a velocidade máxima das rodas
-        max_speed = 0.7  # Ajuste conforme necessário
-        # Se o goleiro estiver apontando para a direção da bola, mova-se diretamente para a bola
-        if math.cos(gk_orientation) * diff_y > 0:
-            gk_v_wheel_0 = max_speed
-            gk_v_wheel_1 = max_speed
-        # Se o goleiro estiver apontando na direção oposta à bola, gire em direção à bola primeiro
-        else:
-            # Calcular a diferença de orientação necessária para apontar para a bola
-            diff_orientation = (
-                math.atan2(ball_pos.y - gk_pos.y, ball_pos.x - gk_pos.x)
-                - gk_orientation
-            )
-            # Normalizar a diferença de orientação para o intervalo [-pi, pi]
-            diff_orientation = math.atan2(
-                math.sin(diff_orientation), math.cos(diff_orientation)
-            )
-            # Definir as velocidades das rodas para girar em direção à bola
-            gk_v_wheel_0 = -max_speed * math.sign(diff_orientation)
-            gk_v_wheel_1 = max_speed * math.sign(diff_orientation)
+        # Calcula a diferença de velocidade baseado no sinal de diff_ang 
+        if diff_ang > 0:
+            # Manipula a roda direita 
+            gk_v_wheel = [ 1, np.cos( diff_ang) ]
+        elif diff_ang < 0:
+            # Manipula a roda esquerda  
+            gk_v_wheel = [ np.cos( diff_ang), 1 ]
+        else: 
+            # Anda reto com velocidade máxima 
+            gk_v_wheel = [ 1, 1 ]
 
-        #return gk_v_wheel_0, gk_v_wheel_1
-        return 1, 1
+        # Ajustar a velocidade máxima conforme necessário, saida de [-1, 1]
+        max_speed = robot2ball_mag*20
+        # print( gk_v_wheel )
+
+        return [ max_speed * gk_v for gk_v in gk_v_wheel ]
+        # return [ 255, 100 ]
+    
+    
 
     def _get_commands(self, actions):
         commands = []
